6e51bfe3aa1779487c7f3f737cbcadee3bb9acc4
Daniel Kudinov <danielkudinov17@gmail.com>
2024-07-14 22:09:51 +0300

    add localStorage and info block

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,9 @@ import Drawer from './components/Drawer.vue';
 
 const items = ref([])
 const cart = ref([])
-
 const drawerOpen = ref(false)
+const isCreatingOrder = ref(false)
+
 
 const totalPrice = computed(
   ()=>cart.value.reduce((acc,item)=> acc+item.price,0)
@@ -21,6 +22,23 @@ const closeDrawer = () => {
 }
 const openDrawer = () => {
   drawerOpen.value = true
+}
+
+const createOrder = async () => {
+  try{
+    isCreatingOrder.value = true
+    const {data} = await axios.post('https://f7715800e56706b7.mokky.dev/orders',{
+      items: cart.value,
+      totalPrice: totalPrice.value,
+    })
+    cart.value = []
+
+    return data;
+  }catch(e){
+    console.log(e)
+  }finally{
+    isCreatingOrder.value = false
+  }
 }
 
 const onClickAddPlus = (item) => {
@@ -124,10 +142,26 @@ const fetchItems = async () => {
 } 
 
 onMounted( async ()=>{
+  const localCart = localStorage.getItem('cart')
+  cart.value = localCart ?  JSON.parse(localCart) : []
+
   await fetchItems()
   await fetchFavorite()
 })
 watch(filter, fetchItems)
+
+watch(cart, ()=>{
+  items.value= items.value.map((item)=>({
+    ...item,
+    isAdded: false
+  }))
+})
+
+watch(cart,()=>{
+  localStorage.setItem('cart', JSON.stringify(cart.value))
+}, {
+  deep:true
+})
 
 provide('cart', {
   cart,
@@ -139,7 +173,7 @@ provide('cart', {
 </script>
 
 <template>
-  <Drawer v-if="drawerOpen" :totalPrice="totalPrice" :varPrice="varPrice"/>
+  <Drawer v-if="drawerOpen" :totalPrice="totalPrice" :varPrice="varPrice" @create-order="createOrder" :is-creating-order="isCreatingOrder.value"/>
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-10 mb-10">
     <Header :total-price="totalPrice" @open-drawer="openDrawer" />
 
@@ -173,7 +207,3 @@ provide('cart', {
     </div>
   </div>
 </template>
-
-<style scoped>
-
-</style>
--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,47 +1,67 @@
 <script setup>
 import DrawerHead from './DrawerHead.vue';
 import CartItemList from './CartItemList.vue';
+import { computed } from 'vue';
+import InfoBlock from './infoBlock.vue';
+
+const emit = defineEmits(['createOrder'])
 
 defineProps({
     totalPrice:Number,
     varPrice:Number,
+    isCreatingOrder: Boolean,
 })
+
+// const buttonDisabled = computed(()=> props.isCreatingOrder ? true : props.totalPrice ? false : true) 
 </script>
 
 <template>
     <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-60"></div>
     <div class="bg-white w-96 h-full fixed  right-0 top-0 z-20 p-8">
         <DrawerHead/>
-        <CartItemList/>
 
-        <div class="flex flex-col gap-4 mt-7 ">
-
-            <div class="flex gap-2 ">
-                <span>
-                    Итого:
-                </span>
-                <div class="flex-1 border-b border-dashed mb-1"></div>
-                <b>
-                    {{ totalPrice }} руб.
-                </b>
-            </div>
-
-            <div class="flex gap-2 ">
-                <span>
-                    Налог 5%:
-                </span>
-                <div class="flex-1 border-b border-dashed mb-1"></div>
-                <b>
-                    {{ varPrice }}
-                </b>
-            </div>
-
+        <div v-if="!totalPrice" class="flex h-full items-center">
+            <InfoBlock 
+                title="Корзина пустая" 
+                description="Добавьте хотябы одну пару кроссовок, чтобы сделать заказ." 
+                image-url="/package-icon.png"
+            />
         </div>
 
-        <button
-        disabled=""
-        class="bg-lime-500 w-full mt-4 cursor-pointer rounded-xl py-3 text-white disabled:bg-slate-400 hover:bg-lime-600 active:bg-lime-700">
-            Оформить заказ
-        </button>
+        <div v-else>
+            <CartItemList v-if="totalPrice"/>
+
+            <div v-if="totalPrice" class="flex flex-col gap-4 mt-7 ">
+                <div class="flex gap-2 ">
+                    <span>
+                        Итого:
+                    </span>
+                    <div class="flex-1 border-b border-dashed mb-1"></div>
+                    <b>
+                        {{ totalPrice }} руб.
+                    </b>
+                </div>
+
+                <div class="flex gap-2 ">
+                    <span>
+                        Налог 5%:
+                    </span>
+                    <div class="flex-1 border-b border-dashed mb-1"></div>
+                    <b>
+                        {{ varPrice }} руб.
+                    </b>
+                </div>
+
+            </div>
+
+            <button
+                :disabled="totalPrice ? false : true"
+                @click="()=>emit('createOrder')"
+                class="bg-lime-500 w-full mt-4 cursor-pointer rounded-xl py-3 text-white disabled:bg-slate-400 hover:bg-lime-600 active:bg-lime-700"
+            >
+                Оформить заказ
+            </button>
+            
+        </div>
     </div>
 </template>
